2c03be50ae87b5c9047e1b59abe05ee05c778909
fawzisilver <90504301+fawzisilver@users.noreply.github.com>
2024-08-01 21:16:46 -0600

    working on weather api (unfinished)

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import search_icon from '../assets/search.png'
 import clear_icon from  '../assets/clear.png'
 import cloud_icon from '../assets/cloud.png'
@@ -10,6 +10,24 @@ import wind_icon from '../assets/wind.png'
 
 
 const Weather = () => {
+
+  const search = async (city) => {
+    try {
+      const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${import.meta.env.VITE_APP_ID}`
+
+      const response = await fetch(url);
+      const data = await response.json();
+
+      console.log(data);
+    } catch (error) {
+ 
+    }
+  }
+
+  useEffect(()=>{
+    search("London");
+  }, [])
+
   return (
     <div className='w-96 place-self-center p-10 border border-black rounded-xl bg-custom-gradient1 '>
         <div className='flex gap-3 justify-center items-center'>
